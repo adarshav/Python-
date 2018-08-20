cec9e28a7466820013cd263562c4144f8c83189b
adarsha <adarshav957@gmail.com>
2018-08-20 10:10:10 +0530

    practice on for loop

--- conditions.py
+++ conditions.py
@@ -7,15 +7,30 @@
 #     print("it is zero");
 
 #accept anumber and check if number is multiple of 3if it is 6,if it is 9
-number = int(input("enter the number"));
-if(number % 3 == 0):
-    if(number % 6 == 0):
-        if(number % 9 == 0):
-            print(number, "will be divided from 3, 6, 9");
-else:
-    print(number, "is not dvisible by 3");
+# number = int(input("enter the number"));
+# if(number % 3 == 0):
+#     if(number % 6 == 0):
+#         if(number % 9 == 0):
+#             print(number, "will be divided from 3, 6, 9");
+# else:
+#     print(number, "is not dvisible by 3");
 
 #write a python program to check whether the given year is leap year or not
  
+# year = int(input("enter the year"));
+# if(year % 4 == 0):
+#     if(year % 100 != 0):
+#         print(year, "is a leap year");
+#     else:
+#         print(year, "is not a leap year");
+# else:
+#     print(year, "Dear, is not at all a leap year");
 
-
+num = int(input("enter the number"));
+sum = 0;i=1;
+while(i <= num):
+    sum = sum + i;
+    i = i+1;
+else:
+    print("you are out of the loop");
+print(sum);
---
+++ loops.py
@@ -0,0 +1,30 @@
+# for letter in 'Python':
+#     print("current letter is", letter);
+
+# numbers = [1,2, 3,4, 5,6, 7, 8,9, 10];
+# sum = 0;
+# for number in numbers:
+#     sum = sum + number;
+# print(sum);
+
+# x = range(1, 20, 2);
+# for n in x:
+#     print("n",n);
+
+#print only even numbers from 1 to 100
+# n = range(2, 100, 2);
+# for i in n:
+#     print("i",i);
+
+# print only odd numbers from 1 to 1000
+# num = range(1, 1000, 2);
+# for i in num:
+#     print("i", i);
+
+number = range(0, 100, 5);
+l1 = [];
+lists = list((number));
+for list in lists:
+    l1.append(list);
+print(l1);
+    
