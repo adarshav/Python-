9a8333e5360eace27811187e397975f670759bff
adarsha <adarshav957@gmail.com>
2018-08-20 09:18:44 +0530

    program on conditions

---
+++ conditions.py
@@ -0,0 +1,21 @@
+# number = int(input("enter the number"));
+# if(number > 0):
+#     print("it is positive");
+# elif(number < 0):
+#     print("it is negative");
+# else:
+#     print("it is zero");
+
+#accept anumber and check if number is multiple of 3if it is 6,if it is 9
+number = int(input("enter the number"));
+if(number % 3 == 0):
+    if(number % 6 == 0):
+        if(number % 9 == 0):
+            print(number, "will be divided from 3, 6, 9");
+else:
+    print(number, "is not dvisible by 3");
+
+#write a python program to check whether the given year is leap year or not
+ 
+
+
