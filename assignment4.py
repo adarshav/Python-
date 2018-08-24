c353d8088d5e9a4665f43ed3cc9f256aa047b917
adarsha <adarshav957@gmail.com>
2018-08-24 18:12:27 +0530

    format

---
+++ assignment4.py
@@ -0,0 +1,18 @@
+print(format(123, "d"));
+print(format(123.45678, "f"));
+print(format(123, "b"));
+print(format(1234, "*>+7,d"));
+print(format(123.45678, "^-09.3f"));
+print("the number is {:d}".format(123));
+print("the number is {:f}".format(123.4546));
+print("the bin {0:b}, {0:o}, hex {0:x}".format(123));
+print("{:5d}".format(12));
+print("{:2d}".format(1234));
+print("{:8.3f}".format(123.34567));
+print("{:05d}".format(12));
+
+
+
+
+
+
